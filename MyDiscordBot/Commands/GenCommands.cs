9d2cdb7ad1f94bc22974ffb72255518c9bb664b2
jbcostan <jbcostan@hotmail.com>
2020-05-08 04:23:26 -0400

    get basic

--- MyDiscordBot/Commands/GenCommands.cs
+++ MyDiscordBot/Commands/GenCommands.cs
@@ -10,13 +10,16 @@ using System.IO;
 using Newtonsoft.Json;
 using RiotSharp.Misc;
 using System.Linq;
+using RiotSharp.Endpoints.ChampionEndpoint;
+using RiotSharp.Endpoints.SummonerEndpoint;
+using RiotSharp.Endpoints.StaticDataEndpoint.Champion;
 
 namespace MyDiscordBot.Commands
 {
     public class GenCommands : BaseCommandModule
     {
 
-        public string riotKey(string uname) {
+        public string riotKey() {
             var json = string.Empty;
             using (var fs = File.OpenRead("config.json"))
             using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
@@ -27,20 +30,41 @@ namespace MyDiscordBot.Commands
             return myriotKey;
                 
         }
-
+        
         [Command("lol")]
-        [Description("get riot stuff")]
-        public async Task GetRiot(CommandContext ctx, params string[] uname)
+        [Description("get lvl")]
+        public async Task GetRiot(CommandContext ctx, [Description("get username")]params string[] uname)
         {
             Console.WriteLine(uname.ToString());
             var username = string.Join(" ", uname);
-            var api = RiotApi.GetDevelopmentInstance(riotKey(username));
+            var api = RiotApi.GetDevelopmentInstance(riotKey());
 
             var summoner = api.Summoner.GetSummonerByNameAsync(Region.Na, username ).Result;
             var name = summoner.Name;
             var level = summoner.Level;
             var accountId = summoner.AccountId;
+
+           
             await ctx.Channel.SendMessageAsync("Level: "+level.ToString()).ConfigureAwait(false);
+        }
+
+        [Command("rotation")]
+        [Description("current champion rotation(NA)")]
+        public async Task ChampRotation(CommandContext ctx)
+        {
+            var api = RiotApi.GetDevelopmentInstance(riotKey());
+            var rotation = api.Champion.GetChampionRotationAsync(Region.Na).Result;
+            var champs = api.Status;
+            
+            //var f2p = string.Empty;
+            //var f2pnewbs = string.Empty;
+            var f2p = rotation.FreeChampionIds.Count;
+            var f2pnewbs = rotation.FreeChampionIdsForNewPlayers.Count;
+            //rotation.FreeChampionIds.ForEach(el => f2p=string.Join(",",el));
+            //rotation.FreeChampionIdsForNewPlayers.ForEach(eln=>f2pnewbs=string.Join(",",eln));
+
+            await ctx.Channel.SendMessageAsync("Current Rotations: "+f2p.ToString()+"\n"+
+            "New Players Rotations: "+f2pnewbs.ToString()+"\n"+champs.ToString()).ConfigureAwait(false);
         }
 
 
